d2fed1425303b3d8d812f282b0b9e788bbf96aed
WillDevAc <wydemairomsilva@gmail.com>
2023-09-26 22:17:52 -0300

    fix

--- src/components/TopUserRanking/index.tsx
+++ src/components/TopUserRanking/index.tsx
@@ -18,7 +18,7 @@ const TopUserRanking: React.FC<ITopUserRanking> = ({
     <li className={S.li}>
       {position === 1 && <img id={S.crown} src="crown.svg" alt="coroa" />}
       <div className={S.user}>
-        <h1>{position}</h1>
+        <h1>#{position}</h1>
       </div>
       <p data-name-podium>{name}</p>
       <span data-revenue-podium className={S.revenue}>
