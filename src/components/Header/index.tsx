dfe76e1ba093254f23d709dd51b581da56571847
WillDevAc <wydemairomsilva@gmail.com>
2023-09-26 21:40:12 -0300

    more details

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import S from './styles.module.scss';
+import { ShoppingCart } from "@phosphor-icons/react";
+
+import S from "./styles.module.scss";
 
 const Header: React.FC = () => {
   return (
     <header className={S.header}>
-        <img src="logo.svg" alt="Logo Lendas Bet" />
-        <button>Solicitar premiação</button>
+      <img src="logo.svg" alt="Logo Lendas Bet" />
+      <button>
+        <ShoppingCart size={20} />
+        Marketplace
+      </button>
     </header>
   );
-}
+};
 
 export default Header;
--- src/components/RankingList/index.tsx
+++ src/components/RankingList/index.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-
-import S from "./styles.module.scss";
-
 import TopUserRanking from "../TopUserRanking";
 import OtherUserRanking from "../OtherUserRanking";
-
 import { useQuery } from "react-query";
-
 import axios from "axios";
+
+import S from "./styles.module.scss";
 
 interface IRankListResponse {
   user: string;
@@ -16,11 +13,11 @@ interface IRankListResponse {
   surname: string;
   position: string;
 }
+
 const RankingList: React.FC = () => {
   const [topRankingList, setTopRankingList] = useState<IRankListResponse[]>([]);
-  const [otherRankingList, setOtherRankList] = useState<IRankListResponse[]>(
-    []
-  );
+  const [otherRankingList, setOtherRankList] = useState<IRankListResponse[]>([]);
+  const [showMore, setShowMore] = useState<number>(10);
 
   const { isLoading } = useQuery("getRankingList", () => {
     return axios
@@ -35,13 +32,18 @@ const RankingList: React.FC = () => {
     console.log("carregando...");
   }
 
+  const handleShowMore = () => {
+    setShowMore((prevShowMore) => prevShowMore + 10);
+  };
+
   return (
     <main className={S.RankingContainer}>
       <section className={S.topRanking}>
         <div className={S.podium}>
           <ul>
-            {topRankingList.map((list) => (
+            {topRankingList.slice(0, showMore).map((list, index) => (
               <TopUserRanking
+                key={index}
                 name={list.user}
                 surname={list.surname}
                 position={parseInt(list.position)}
@@ -53,14 +55,20 @@ const RankingList: React.FC = () => {
       </section>
       <div className={S.otherRanking}>
         <ul data-aos="fade-right">
-          {otherRankingList.map((list) => (
+          {otherRankingList.slice(0, showMore).map((list, index) => (
             <OtherUserRanking
+              key={index}
               position={parseInt(list.position)}
               name={list.user}
               revenue={list.deposit_formatted}
             />
           ))}
         </ul>
+        {showMore < otherRankingList.length && (
+          <div className={S.More}>
+            <button onClick={handleShowMore}>Mostrar mais resultados</button>
+          </div>
+        )}
       </div>
     </main>
   );
