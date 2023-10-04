9a33dcd4afaa537cad7399713004536c8f1f0723
WillDevAc <wydemairomsilva@gmail.com>
2023-10-04 02:19:03 -0300

    feat: finish

--- src/components/RankingList/index.tsx
+++ src/components/RankingList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 import S from "./styles.module.scss";
@@ -18,22 +18,32 @@ interface IRankListResponse {
 
 const RankingList: React.FC = () => {
   const [topRankingList, setTopRankingList] = useState<IRankListResponse[]>([]);
-  const [otherRankingList, setOtherRankList] = useState<IRankListResponse[]>(
-    []
-  );
-  const [showMore, setShowMore] = useState<number>(10);
+  const [otherRankingList, setOtherRankList] = useState<IRankListResponse[]>([]);
+  const [showMore] = useState<number>(10);
   const [visible, setVisible] = useState(false);
-  const [selectedUserData, setSelectedUserData] =
-    useState<IRankListResponse | null>(null);
+  const [selectedUserData, setSelectedUserData] = useState<IRankListResponse | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>(""); 
+  const [visibleResults, setVisibleResults] = useState<number>(10); 
+  const [noResults, setNoResults] = useState<boolean>(false); 
 
   const { isLoading } = useQuery("getRankingList", () => {
-    return axios
-      .get("https://fraternidadesim.com/backend/")
-      .then((response) => {
-        setTopRankingList(response.data.TopRanking);
-        setOtherRankList(response.data.Others);
-      });
+    return axios.get("https://fraternidadesim.com/backend/").then((response) => {
+      setTopRankingList(response.data.TopRanking);
+      setOtherRankList(response.data.Others);
+    });
   });
+
+  useEffect(() => {
+    setVisibleResults(10);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    setNoResults(
+      otherRankingList.filter((list) =>
+        list.user.toLowerCase().includes(searchTerm.toLowerCase())
+      ).length === 0
+    );
+  }, [otherRankingList, searchTerm]);
 
   const showModalRewards = (userData: IRankListResponse | null) => {
     setSelectedUserData(userData);
@@ -42,7 +52,7 @@ const RankingList: React.FC = () => {
   };
 
   const handleShowMore = () => {
-    setShowMore((prevShowMore) => prevShowMore + 10);
+    setVisibleResults((prevVisibleResults) => prevVisibleResults + 10);
   };
 
   return (
@@ -50,7 +60,7 @@ const RankingList: React.FC = () => {
       <main className={S.RankingContainer}>
         <section className={S.topRanking}>
           <div className={S.podium}>
-          <h1>Ranking afiliados</h1>
+            <h1>Ranking afiliados</h1>
             <ul>
               {topRankingList.slice(0, showMore).map((list, index) => (
                 <TopUserRanking
@@ -65,28 +75,44 @@ const RankingList: React.FC = () => {
           </div>
         </section>
         <div className={S.otherRanking}>
+          <div className={S.otherRankingSearch}>
+            <input
+              type="search"
+              placeholder="Pesquisar afiliado..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
           <ul data-aos="fade-right">
-            {otherRankingList.slice(0, showMore).map((list, index) => (
-              <div key={index} onClick={() => showModalRewards(list)}>
-                <OtherUserRanking
-                  position={parseInt(list.position)}
-                  name={list.user}
-                  revenue={list.deposit_formatted}
-                />
-              </div>
-            ))}
+            {otherRankingList
+              .filter((list) =>
+                list.user.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .slice(0, visibleResults)
+              .map((list, index) => (
+                <div key={index} onClick={() => showModalRewards(list)}>
+                  <OtherUserRanking
+                    position={parseInt(list.position)}
+                    name={list.user}
+                    revenue={list.deposit_formatted}
+                  />
+                </div>
+              ))}
           </ul>
-          {showMore < otherRankingList.length && (
+          {searchTerm === "" && visibleResults < otherRankingList.length && (
             <div className={S.More}>
               <button onClick={handleShowMore}>Mostrar mais resultados</button>
-              {visible && (
+            </div>
+          )}
+          {visible && (
                 <ModalRewards
                   setVisible={setVisible}
                   visible={visible}
                   {...selectedUserData}
                 />
               )}
-            </div>
+          {noResults && searchTerm !== "" && (
+            <h5>Nenhum afiliado encontrado.</h5>
           )}
         </div>
       </main>
